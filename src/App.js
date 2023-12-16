611ce779de4d2ee33ca66bd874afea975b90c0f8
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2023-12-16 22:15:40 +0530

    task 10

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   const expenses = [
-    {id: 'e1',title: 'Rent',amount: 990,date: new Date(2020, 7, 14),},
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {id: 'e3', title: 'Car Insurance',amount: 294.67,date: new Date(2021, 2, 28), },
-    { id: 'e4',title: 'New Desk (Wooden)',amount: 450,date: new Date(2021, 5, 12), }
+    {id: 'e1',title: 'Rent',amount: 990,date: new Date(2020, 7, 14), loc:'mumbai'},
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12),loc:'pune' },
+    {id: 'e3', title: 'Car Insurance',amount: 294.67,date: new Date(2021, 2, 28), loc:'delhi'},
+    { id: 'e4',title: 'New Desk (Wooden)',amount: 450,date: new Date(2021, 5, 12),loc:'kop' }
   ]
  
   return (
     <div >
-   <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-   <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-   <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-   <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+      <h2> Expense Tracker</h2>
+   <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} loc={expenses[0].loc}/>
+   <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} loc={expenses[1].loc}/>
+   <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} loc={expenses[2].loc}/>
+   <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} loc={expenses[3].loc}/>
    
    
     </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,17 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css' ;
 
 function ExpenseItem(props) {
-   const expenseDate = new Date(2023,0,22);
-  const expenseTitle = 'Car Insurnase';
-  const expenseAmount = 300;
-  const location = 'Mumbai'
+   
 
   return (
     <div className='expense-item'>
-      <div > {props.date.toDateString()} </div>
+      <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
         <h2>{props.title} </h2>
       </div>
       <div className='expense-item__price'> { props.amount}</div>
+      <div className='exp-item-location'> {props.loc}</div>
      
     </div>
   );
