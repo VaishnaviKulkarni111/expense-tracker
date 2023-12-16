4386de6fda633925ea38917695107d316dec8c8c
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2023-12-16 14:55:27 +0530

    task 9

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import './ExpenseItem.css' ;
 
-function ExpenseItem() {
+function ExpenseItem(props) {
    const expenseDate = new Date(2023,0,22);
   const expenseTitle = 'Car Insurnase';
   const expenseAmount = 300;
-
+  const location = 'Mumbai'
 
   return (
     <div className='expense-item'>
-      <div > {expenseDate.toDateString()} </div>
+      <div > {props.date.toDateString()} </div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle} </h2>
+        <h2>{props.title} </h2>
       </div>
-      <div className='expense-item__price'> { expenseAmount}</div>
+      <div className='expense-item__price'> { props.amount}</div>
      
     </div>
   );
