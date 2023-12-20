f17126872c52becd30422e9c93060059ed210551
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2023-12-20 13:47:19 +0530

    adv react task 4

--- src/components/NewExpense/Form.js
+++ src/components/NewExpense/Form.js
@@ -1,9 +1,22 @@
+import React, {useState } from 'react'
 import './Form.css'
 
 const Form1 =() => {
+ const [enteredTitle, setEnteredTitle] = useState('');
+ const [enteredAmount, setEnteredAmount] = useState('');
+ const [enteredDate, setEnteredDate] = useState('');
+
     const titleChangeHandler = (event) =>{
-        console.log(event.target.value)
-    }
+        setEnteredTitle(event.target.value)
+    };
+
+    const amountChangeHandler = (event) => {
+         setEnteredAmount(event.target.value)
+    };
+    const dateChangeHandler = (event)=> {
+        setEnteredDate(event.target.value)
+    } ; 
+
     return (
   <form>
     <div className='new-expense__controls '>
@@ -12,12 +25,12 @@ const Form1 =() => {
             <input  type='text' onChange={titleChangeHandler}/>
         </div>
         <div className='new-expense__control'>
-            <label> Amount</label>
-            <input  type='number' min="1"/>
+            <label> Amount </label>
+            <input  type='number' min="1" onChange={amountChangeHandler}/>
         </div>
         <div className='new-expense__control'>
             <label> Date</label>
-            <input  type='date' min="2019-01-01"/>
+            <input  type='date' min="2019-01-01" onChange={dateChangeHandler}/>
         </div>
         <div className='new-expense__actions'>
              <button type='submit'> Add Expense</button>
