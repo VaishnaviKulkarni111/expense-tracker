25b54d99756e68cce25310b7a028edc2c0605ec3
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2023-12-22 14:06:48 +0530

    advR task 11

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const DUMMY_EXPENSES = [
     id: 'e3',title: 'Car Insurance',amount: 294.67,  date: new Date(2021, 2, 28),
   },
   {
-    id: 'e4',title: 'New Desk (Wooden)',amount: 450,date: new Date(2021, 5, 12),
+    id: 'e4',title: 'New Desk (Wooden)',amount: 450,date: new Date(2020, 5, 12),
   },
 ];
 
